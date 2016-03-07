1b02d40afb7ffc95ed7ce4ebaa6b9f63c20c22a0
Hao Ling <lingmengc@outlook.com>
2016-03-07 13:23:14 +0800

    update 7

---
+++ Many.ThirdParty/CustomControls/HomeButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace Many.ThirdParty.CustomControls
+{
+    public sealed class HomeButton : Windows.UI.Xaml.Controls.Button
+    {
+        public HomeButton()
+        {
+            this.DefaultStyleKey = typeof(HomeButton);
+        }
+        
+    }
+}
